1b32595cfef8b02a79189fcc3f9354633923d0b3
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-15 02:24:22 +0300

    Add first main in this log way

---
+++ main.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+def main():
+    # Here we need to init Field and Game logic
+    # Then we need start game 
+    # Maybe if it will be in web, we must start game server
+    # Or we can create some server with a few people, who play PVP
+    print("Hello, this is python chess game")
+
+
+if __name__ == "__main__":
+    main()

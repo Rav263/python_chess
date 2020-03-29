c45af7fa04b1e382776c507f0ac0abec77acb60d
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 11:14:23 +0300

    Init field class

---
+++ field.py
@@ -0,0 +1,8 @@
+class Field:
+    def __init__(self):
+        print("Init field class")
+    
+    def do_turn(self, start_pos, end_pos):
+        print("Doing turn from {0} to {1}".format(start_pos, end_pos))
+
+

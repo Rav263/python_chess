2aa56f49d5488872370f2fc3e7abf0ef05463a4f
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 15:28:53 +0300

    update main to check new functions

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 
 import field
 import gamelogic
+import io_functions
+
 
 def main():
-    plate = field.Field() 
+    data  = io_functions.Data("data.dat")
+    plate = field.Field(data) 
+
+    io_functions.print_field(plate.field, data)
+    plate.do_turn((0,0), (2, 2))
+    io_functions.print_field(plate.field, data)
     # Here we need to init Field and Game logic
     logic = gamelogic.Logic()
     # Then we need start game 

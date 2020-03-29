ba46bf66d8f65f24d9dd9807b5d3eb7f81b3fd3c
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 19:06:35 +0300

    Add Turn class

--- gamelogic.py
+++ gamelogic.py
@@ -1,3 +1,10 @@
+class Trun:
+    def __init__(self, start_pos, end_pos, color):
+        self.start_pos = start_pos
+        self.end_pos = end_pos
+        self.color = color
+
+
 class Logic:
     def __init__(self):
         print("Init game logic class")

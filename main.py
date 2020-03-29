a70a60ddabb08b0b4714cec0529c4946517e3939
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 11:14:59 +0300

    Update main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #! /usr/bin/python3
 
+import field
+import gamelogic
+
 def main():
+    plate = field.Field() 
     # Here we need to init Field and Game logic
+    logic = gamelogic.Logic()
     # Then we need start game 
     # Maybe if it will be in web, we must start game server
     # Or we can create some server with a few people, who play PVP

eb2e4e14a289857d53cffbb152f5e858f04be240
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 22:41:17 +0300

    Update for using Turn class and add field io functions

--- field.py
+++ field.py
@@ -1,17 +1,20 @@
-import io_functions
-
-
 class Field:
     def __init__(self, data):
         print("Init field class")
         self.load_field(data)
 
-    def do_turn(self, start_pos, end_pos):
-        print("Doing turn from {0} to {1}".format(start_pos, end_pos))
+    def do_turn(self, turn):
+        print("Doing turn from {0} to {1}".format(turn.start_pos, turn.end_pos))
 
         # move figure
-        self.field[end_pos[0]][end_pos[1]] = self.field[start_pos[0]][start_pos[1]]
-        self.field[start_pos[0]][start_pos[1]] = 0
+        self.set_map(turn.end_pos, self.get_map(turn.start_pos))
+        self.set_map(turn.start_pos, 0)
 
     def load_field(self, data):
         self.field = data.data["FIELD"]
+
+    def get_map(self, pos):
+        return self.field[pos[0]][pos[1]]
+
+    def set_map(self, pos, value):
+        self.field[pos[0]][pos[1]] = value

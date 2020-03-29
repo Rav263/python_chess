3639f5a81b6274e60621f5721aa503cb5a4ede8f
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 14:09:43 +0300

    Add print function

--- io_functions.py
+++ io_functions.py
@@ -18,3 +18,19 @@ class Data:
                     tmp_str = file.readline()
                     tmp.append([int(i) for i in tmp_str.strip().split()])
                 self.data["FIELD"] = tmp
+            
+            if i.strip() == "FIGURES":
+                tmp = dict()
+                for j in range(13):
+                    tmp_str = file.readline().split()
+                    tmp[int(tmp_str[0])] = tmp_str[1]
+                self.data["FIGURES"] = tmp
+
+    def print_field(self):
+        for i in range(8):
+            print(8 - i, end = " |")
+            for now_fig in self.data["FIELD"][i]:
+                print(self.data["FIGURES"][now_fig], end = " ")
+            print()
+        print("  ------------------------")
+        print("   a  b  c  d  e  f  g  h")

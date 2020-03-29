6e7e8d12b09f6e6a1d928615668d28d49fa5ca52
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 22:43:52 +0300

    Add turn check and fix class name

--- gamelogic.py
+++ gamelogic.py
@@ -1,4 +1,4 @@
-class Trun:
+class Turn:
     def __init__(self, start_pos, end_pos, color):
         self.start_pos = start_pos
         self.end_pos = end_pos
@@ -8,3 +8,11 @@ class Trun:
 class Logic:
     def __init__(self):
         print("Init game logic class")
+
+    def check_turn(self, turn, plane):
+        if (plane.get_map(turn.start_pos) // 10 != turn.color or
+                plane.get_map(turn.start_pos) == 0):
+            return False
+        if plane.get_map(turn.start_pos) == 10:
+            return True
+        return True

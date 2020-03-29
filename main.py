57412b278af8bc7f777026ebd7860e0fac538b81
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-29 22:45:12 +0300

    Update to check new changes

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ import io_functions
 
 
 def main():
+    # Here we need to init Field and Game logic
     data = io_functions.Data("data.dat")
     plate = field.Field(data)
-
-    io_functions.print_field(plate.field, data)
-    plate.do_turn((0, 0), (2, 2))
-    io_functions.print_field(plate.field, data)
-    # Here we need to init Field and Game logic
     logic = gamelogic.Logic()
     # Then we need start game
+
+    color = 1
+
+    while True:
+        io_functions.print_field(plate.field, data)
+        now_turn = io_functions.get_turn(logic, color, plate)
+
+        plate.do_turn(now_turn)
+
+        color = 3 - color
     # Maybe if it will be in web, we must start game server
     # Or we can create some server with a few people, who play PVP
-    print("Hello, this is python chess game")
 
 
 if __name__ == "__main__":
+    print("Hello, this is python chess game")
     main()

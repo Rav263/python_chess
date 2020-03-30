1c65d2a940e5a88126aa0f8bf0549564e75dcd82
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-30 22:04:12 +0300

    Add functions to check maps and const for figures types

--- field.py
+++ field.py
@@ -1,7 +1,16 @@
 class Field:
+    empty_map = 0
+    pown = 1
+    knight = 2
+    bishop = 3
+    rook = 4
+    king = 5
+    queen = 6
+
     def __init__(self, data):
         print("Init field class")
         self.load_field(data)
+        self.field_size = data.field_size
 
     def do_turn(self, turn):
         print("Doing turn from {0} to {1}".format(turn.start_pos, turn.end_pos))
@@ -15,6 +24,27 @@ class Field:
 
     def get_map(self, pos):
         return self.field[pos[0]][pos[1]]
+
+    def get_color_figure(self, figure):
+        return figure // 10
+
+    def get_type_figure(self, figure):
+        return figure % 10
+
+    def get_type_map(self, pos):
+        return self.get_map(pos) % 10
+
+    def get_color_map(self, pos):
+        return self.get_map(pos) // 10
+
+    def check_pos(self, pos):
+        if pos[0] >= self.field_size or pos[0] < 0:
+            return False
+
+        if pos[1] >= self.field_size or pos[1] < 0:
+            return False
+
+        return True
 
     def set_map(self, pos, value):
         self.field[pos[0]][pos[1]] = value

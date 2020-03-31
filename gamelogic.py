0b8c7e5dde19fd3d9988042a482a756752b5f5d3
Никита Никифоров <nickiforov.nik@gmail.com>
2020-03-31 19:22:10 +0300

    Move specific functions in over file

--- gamelogic.py
+++ gamelogic.py
@@ -1,4 +1,5 @@
 import io_functions
+import check_turn as check
 
 
 def difference(tuple_1, tuple_2):
@@ -49,21 +50,21 @@ class Logic:
 
         # pown
         if figure_type == plate.pown:
-            return self.check_pown(turn, plate)
+            return check.check_pown(turn, plate)
 
         coord_diff = difference(turn.start_pos, turn.end_pos)
 
         # knight
         if figure_type == plate.knight:
-            return self.check_knight(*coord_diff)
+            return check.check_knight(*coord_diff)
 
         # bishop
         if figure_type == plate.bishop:
-            return self.check_bishop(turn, *coord_diff, plate)
+            return check.check_bishop(turn, *coord_diff, plate)
 
         # rook
         if figure_type == plate.rook:
-            return self.check_rook(turn, *coord_diff, plate)
+            return check.check_rook(turn, *coord_diff, plate)
 
         # king
         if figure_type == plate.king:
@@ -73,59 +74,7 @@ class Logic:
 
         # queen
         if figure_type == plate.queen:
-            return (self.check_bishop(turn, *coord_diff, plate) or
-                    self.check_rook(turn, *coord_diff, plate))
-
-        return False
-
-    def check_pown(self, turn, plate):
-        if turn.color == 1:
-            if turn.end_pos[1] == turn.start_pos[1] and plate.get_map(turn.end_pos) == 0:
-                tmp = turn.start_pos[0] - turn.end_pos[0]
-                if tmp == 1 or (tmp == 2 and turn.start_pos[0] == 6):
-                    if plate.get_type_map(turn.end_pos) == plate.empty_map:
-                        return True
-        else:
-            if turn.end_pos[1] == turn.start_pos[1]:
-                tmp = turn.end_pos[0] - turn.start_pos[0]
-                if tmp == 1 or (tmp == 2 and turn.start_pos[0] == 1):
-                    if plate.get_type_map(turn.end_pos) == plate.empty_map:
-                        return True
-
-        return False
-
-    def check_knigth(self, coord_diff_x, coord_diff_y):
-        if abs(coord_diff_x) == 1 and abs(coord_diff_y) == 2:
-            return True
-
-        if abs(coord_diff_x) == 2 and abs(coord_diff_y) == 1:
-            return True
-
-        return False
-
-    def check_bishop(self, turn, coord_diff_x, coord_diff_y, plate):
-        if abs(coord_diff_x) == abs(coord_diff_y):
-            sign_x = -1 if coord_diff_x < 0 else 1
-            sign_y = -sign_x if coord_diff_x == -coord_diff_y else sign_x
-
-            for i in range(sign_x, coord_diff_x, sign_x):
-                if (plate.get_type_map(sum(turn.end_pos, (i, i * sign_x * sign_y))) !=
-                        plate.empty_map):
-                    return False
-            return True
-
-        return False
-
-    def check_rook(self, turn, coord_diff_x, coord_diff_y, plate):
-        if (coord_diff_x == 0 and coord_diff_y != 0) or (coord_diff_x != 0 and coord_diff_y == 0):
-            sign_x = -1 if coord_diff_x < 0 else 1 if coord_diff_x != 0 else 0
-            sign_y = -1 if coord_diff_y < 0 else 1 if coord_diff_y != 0 else 0
-
-            for i in range(1, coord_diff_x + coord_diff_y):
-                if (plate.get_type_map(sum(turn.end_pos, (i * sign_x, i * sign_y))) !=
-                        plate.empty_map):
-                    return False
-
-            return True
+            return (check.check_bishop(turn, *coord_diff, plate) or
+                    check.check_rook(turn, *coord_diff, plate))
 
         return False

01fcfd789deee89878dc15874cd57ed40b31daa7
Никита Никифоров <nickiforov.nik@gmail.com>
2020-04-30 19:13:27 +0300

    Add multiprocessing test

--- test_threading.py
+++ test_threading.py
@@ -1,7 +1,12 @@
+#! /usr/bin/python3
+
 from threading import Thread
+from multiprocessing import Process
+from multiprocessing import Manager
+import sys
 
 
-class ThreadRet(Thread):
+class ThreadRet(Process):
     def __init__(self, group=None, target=None, name=None,
                  args=(), kwargs={}, Verbose=None):
         Thread.__init__(self, group, target, name, args, kwargs)
@@ -19,29 +24,30 @@ class ThreadRet(Thread):
         return self._return
 
 
-def sum_fun(arr, start, end):
+def sum_fun(arr, start, end, return_dict):
     summ = 0
     for i in arr[start:end]:
         summ += i
 
-    return summ
+    return_dict[start] = summ
 
 
-plate = [x for x in range(2**21)]
+if __name__ == "__main__":
+    manager = Manager()
+    return_dict = manager.dict()
 
+    plate = [x for x in range(2**21)]
 
-num_threads = int(input("Enter threads num: "))
+    num_threads = int(sys.argv[1])
 
-block_size = len(plate) // num_threads
+    block_size = len(plate) // num_threads
+    threads = []
 
-threads = []
+    for i in range(num_threads):
+        threads.append(Process(target=sum_fun, args=(plate, i * block_size, (i + 1) * block_size, return_dict)))
+        threads[-1].start()
 
-for i in range(num_threads):
-    threads.append(ThreadRet(target=sum_fun, args=(plate, i * block_size, (i + 1) * block_size)))
-    threads[-1].start()
+    for a in threads:
+        a.join()
 
-summ = 0
-for a in threads:
-    summ += a.join()
-
-print(summ)
+    print(sum(return_dict.values()))

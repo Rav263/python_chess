43e967626343b83a1d67f0db5e7bd1fd559a3c50
Никита Никифоров <nickiforov.nik@gmail.com>
2020-05-03 17:56:07 +0300

    Update main for use args

--- main.py
+++ main.py
@@ -3,11 +3,48 @@
 from api import Api
 
 
+import sys
+
+
+def parse_args():
+    parsed_args = dict()
+
+    for index, arg in enumerate(sys.argv):
+        if arg == "--mode":
+            parsed_args["MODE"] = sys.argv[index + 1]
+        if arg == "--help":
+            parsed_args["HELP"] = True
+        if arg == "--difficulty":
+            parsed_args["DIFFICULTY"] = int(sys.argv[index + 1])
+
+    return parsed_args
+
+
+def print_help():
+    print("------ HELP MESSAGE ------")
+    print(" --mode (GUI, CMD)        GUI default, to run in GUI. CMD - to run in text mode")
+    print(" --difficulty (1 - 5)     Set AI difficulty. default 5")
+    print(" --help                   Print this message")
+
+
 def main():
-    api = Api()
-    api.start_cmd()
-    # Maybe if it will be in web, we must start game server
-    # Or we can create some server with a few people, who play PVP
+    parsed_args = parse_args()
+
+    if "HELP" in parsed_args:
+        print_help()
+        return None
+
+    difficulty = 5
+    if "DIFFICULTY" in parsed_args:
+        difficulty = parsed_args["DIFFICULTY"]
+
+    api = Api(difficulty)
+
+    if "MODE" in parsed_args and parsed_args["MODE"] == "CMD":
+        api.start_cmd()
+    else:
+        print("Starting GUI")
+        # GUI START CODE HERE
 
 
 if __name__ == "__main__":

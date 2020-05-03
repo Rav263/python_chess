68ba32e815ec913a0f2880893ae2e179c55176c6
Никита Никифоров <nickiforov.nik@gmail.com>
2020-05-03 17:12:13 +0300

    Add function to get dict of possible turns

--- api.py
+++ api.py
@@ -3,6 +3,9 @@ from gamelogic import Logic
 from io_functions import Data
 
 
+from collections import defaultdict
+
+
 class Api:
     def __init__(self):
         # Here we need to init Field and Game logic
@@ -13,3 +16,14 @@ class Api:
 
     def start_cmd(self):
         self.logic.start(self.board, self.data)
+
+    def get_possible_turns(self, color):
+        possible_turns = self.logic.generate_all_possible_turns(self.board, color)
+
+        turns = defaultdict(list)
+
+        for end_pos in possible_turns:
+            for start_pos in possible_turns[end_pos]:
+                turns[start_pos].append(end_pos)
+
+        return turns

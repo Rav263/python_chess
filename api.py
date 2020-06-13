2147ec6a69fdd5fc00e7310f4b679c33c4e6c96e
Никита Никифоров <nickiforov.nik@gmail.com>
2020-06-13 16:32:01 +0300

    Update turn intarface and add new game function

--- api.py
+++ api.py
@@ -3,10 +3,9 @@
 from collections import defaultdict
 from board import Board
 from gamelogic import Logic
-from gamelogic import Turn
 from io_functions import Data
 from evaluate import Evaluate
-
+from turns import read_nodes
 
 class Api:
     """backend api class to ui"""
@@ -16,7 +15,8 @@ class Api:
         self.data = Data("data.dat")
         self.board = Board(self.data)
         self.evaluate = Evaluate("eval_coofs.dat")
-        self.logic = Logic(self.data, threads, self.evaluate)
+        self.debuts = read_nodes()
+        self.logic = Logic(self.data, threads, self.evaluate, self.debuts)
         self.difficulty = difficulty
         self.turn_index = 0
         print(self.evaluate.evaluate_board_mg(self.board, 1))
@@ -28,7 +28,11 @@ class Api:
 
     def get_possible_turns(self, color):
         """returns possible turns from backend"""
-        possible_turns = self.logic.generate_all_possible_turns(self.board, color)
+        if self.turn_index != 0:
+            last_turn = self.logic.turn_history[self.turn_index - 1][0]
+        else:
+            last_turn = self.logic.NULL_TURN
+        possible_turns = self.logic.generate_all_possible_turns(self.board, color, last_turn)
 
         turns = defaultdict(list)
 
@@ -52,12 +56,11 @@ class Api:
 
         return -1
 
-    def do_turn(self, start, end, pawn=0, castling=False):
+    def do_turn(self, start, end, pawn=0):
         """doing User turn
         start    -- start position
         end      -- end position
         pawn     -- figure type if pawn get transformation
-        castling -- flag if turn is castling
         """
 
         color = self.board.get_color_map(start)
@@ -65,20 +68,33 @@ class Api:
         if self.turn_index != len(self.logic.turn_history):
             self.logic.turn_history = self.logic.turn_history[:self.turn_index]
 
-        if castling:
-            if start[1] > end[1]:
-                start = (*start, (start[0], 0), (start[0], 3))
-            else:
-                start = (*start, (start[0], 7), (start[0], 5))
+        last_turn = self.logic.NULL_TURN
+        if self.turn_index != 0:
+            last_turn = self.logic.turn_history[self.turn_index - 1][0]
+        
+        all_turns = self.logic.generate_turns(self.board, color, last_turn)
 
-        now_turn = Turn(start, end, color, color * 10 + pawn, castling)
+        for tmp_turn in all_turns:
+            if tmp_turn.start_pos[:2] == start and tmp_turn.end_pos[:2] == end:
+                now_turn = tmp_turn
+                break
+
+        if pawn != 0:
+            now_turn.pawn = pawn
+
         tmp, flags = self.board.do_turn(now_turn)
 
         self.logic.add_turn_to_history((now_turn, tmp, flags))
         self.turn_index += 1
 
-    def ai_turn(self, color, last_turn):
+    def ai_turn(self, color):
         """ai do turn with color"""
+        
+        if self.turn_index != 0:
+            last_turn = self.logic.turn_history[self.turn_index - 1][0]
+        else:
+            last_turn = self.logic.NULL_TURN
+
         now_turn = self.logic.root_ai_turn(self.board, color, self.difficulty, last_turn)[0]
         tmp, flags = self.board.do_turn(now_turn)
 
@@ -86,6 +102,12 @@ class Api:
 
         self.turn_index += 1
         return now_turn
+
+    def start_new_game(self):
+        self.board = Board(self.data)
+        print("WTF")
+        self.logic.debuts = self.debuts
+        self.logic.flag = True
 
     def previous_turn(self):
         """returns previous turn and un do it on board"""

d3d70fdbf10b0d138329f3f15cbac5739a11c82f
Никита Никифоров <nickiforov.nik@gmail.com>
2020-06-13 17:46:07 +0300

    Update io functions

--- io_functions.py
+++ io_functions.py
@@ -2,9 +2,6 @@
 import sys
 
 
-import gamelogic
-
-
 class Data:
     """Class Data for load config file and get info from it"""
     data = dict()
@@ -99,67 +96,72 @@ def print_board(board, data):
     print()
 
 
-def check_diff(start_pos, end_pos):
-    return abs(start_pos[1] - end_pos[1]) > 1
+def get_color():
+    while True:
+        color = input("Please enter your color (white\\black): ").strip().lower()
+        if color in ("white", "black"):
+            break
+    return 1 if color == "white" else 2
 
 
-def get_turn(logic, color, board, last_turn):
+def new_game():
+    while True:
+        game = input("Do you whant another game (y\\n):").strip().lower()
+        if game in ("y", "n"):
+            break
+    return game == "y"
+
+
+def get_turn(color, api):
     """get_turn(logic, color, board) -> Turn
 
     Get turn from user
     """
-    possible_turns = logic.generate_all_possible_turns(board, color, last_turn)
+    possible_turns = api.get_possible_turns(color)
 
     if len(possible_turns) == 0:
-        return logic.NULL_TURN
+        return ((-1, -1), (-1, -1), -1)
 
     while True:
         line = input("Please enter your turn: ").strip()
         if len(line) not in (4, 5):
             print("Wrong format")
             continue
+        if ord(line[0]) < ord("a") or ord(line[0]) > ord("h"):
+            print("Wrong format")
+            continue
+        if ord(line[2]) < ord("a") or ord(line[2]) > ord("h"):
+            print("Wrong format")
+            continue
+        if ord(line[1]) < ord("1") or ord(line[1]) > ord("8"):
+            print("Wrong format")
+            continue
+        if ord(line[3]) < ord("1") or ord(line[3]) > ord("8"):
+            print("Wrong format")
+            continue
 
-        start_pos = (board.board_size - int(line[1]), abs(ord(line[0]) - ord("a")))
-        end_pos = (board.board_size - int(line[3]), abs(ord(line[2]) - ord("a")))
+        start_pos = (api.board.board_size - int(line[1]), abs(ord(line[0]) - ord("a")))
+        end_pos = (api.board.board_size - int(line[3]), abs(ord(line[2]) - ord("a")))
 
+        fig_num = 0
         if len(line) == 5:
             figure = line[4]
-            fig_num = 0
             if figure == "Q":
-                fig_num = board.queen
+                fig_num = api.board.queen
             elif figure == "K":
-                fig_num = board.knight
+                fig_num = api.board.knight
             elif figure == "R":
-                fig_num = board.rook
+                fig_num = api.board.rook
             elif figure == "B":
-                fig_num = board.bishop
+                fig_num = api.board.bishop
             else:
+                print("Undefined promotion figure")
                 continue
-
-            now_turn = gamelogic.Turn(start_pos, end_pos, color, pawn=(color*10+fig_num))
-
-            if end_pos in possible_turns:
-                if (*start_pos, now_turn.pawn) in possible_turns[end_pos]:
-                    break
-        else:
-            if board.get_king_pos(color) == start_pos and check_diff(start_pos, end_pos):
-                print(start_pos, end_pos)
-                if start_pos[1] > end_pos[1]:
-                    start_pos = (*start_pos, (start_pos[0], 0), (start_pos[0], 3))
-                else:
-                    start_pos = (*start_pos, (start_pos[0], 7), (start_pos[0], 5))
-
-                print(start_pos, possible_turns[end_pos])
-                now_turn = gamelogic.Turn(start_pos, end_pos, color, castling=True)
-            elif board.get_type_map(start_pos) == board.pawn and board.get_type_map(end_pos) == board.empty_map and abs(start_pos[1] - end_pos[1]) == 1:
-                now_turn = gamelogic.Turn(start_pos, end_pos, color, passant=True)
-                end_pos = (*end_pos, True)
-            else:
-                now_turn = gamelogic.Turn(start_pos, end_pos, color)
-
-            if end_pos in possible_turns and start_pos in possible_turns[end_pos]:
+        if start_pos in possible_turns:
+            if end_pos in possible_turns[start_pos]:
                 break
-
-        print("Wrong Turn, try again")
-
-    return now_turn
+            else:
+                print("Not possible turn")
+        else:
+            print("Not possible turn")
+    return (start_pos, end_pos, fig_num)

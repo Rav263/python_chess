986aadc768d008db77a40d81620e34fec8c95abb
p0l0satik <denis.cucu@yandex.ru>
2020-06-18 15:53:09 +0300

    fixed bug in docs

--- math_functions.py
+++ math_functions.py
@@ -11,10 +11,6 @@ def difference(tuple_1, tuple_2):
     :return: tuple_1 - tuple_2
     :rtype: (int, int)
     """
-    difference(tuple_1, tuple_2) -> tuple
-
-    returns tuple difference
-    
 
     return (tuple_1[0] - tuple_2[0], tuple_1[1] - tuple_2[1])
 

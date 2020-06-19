9530e1ae89d198b3240dd51bb3ebd58281af9326
Никита Никифоров <nickiforov.nik@gmail.com>
2020-06-19 07:15:03 +0300

    Update for internalization

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=["PythonChess"],
     package_dir={"PythonChess": "PythonChess"},
     install_requires=["tqdm", "pyqt5"],
-    package_data={"PythonChess": ["./*.dat", "./styles.qss",
+    package_data={"PythonChess": ["./*.dat", "./styles.qss", "ru/LC_MESSAGES/*.mo",
                                   "./images/*", "./images/beat/*", "./images/merida/*",
                                   "./images/moved/*", "./images/pressed/*"]},
 )
